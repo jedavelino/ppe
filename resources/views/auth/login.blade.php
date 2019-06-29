9d8927a07c79bcb7014dee06c59187135daed755
Jedidiah <jedidiah@mesasix.com>
2019-06-29 21:16:42 +0800

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,72 +2,50 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+	<div class="w-full max-w-xs mx-auto">
+			<div class="text-xl font-medium">{{ __('Login') }}</div>
+			<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mt-4" method="POST" action="{{ route('login') }}">
+					@csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+					<div class="">
+							<label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('E-Mail Address') }}</label>
+							<input id="email" type="email" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+							@error('email')
+									<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+									</span>
+							@enderror
+					</div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+					<div class="mt-4">
+							<label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Password') }}</label>
+							<input id="password" type="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+							@error('password')
+									<span class="invalid-feedback" role="alert">
+											<strong>{{ $message }}</strong>
+									</span>
+							@enderror
+					</div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+					<div class="flex items-center mt-4">
+							<label class="md:w-2/3 block text-gray-700 text-sm font-bold" for="remember">
+									<input class="mr-2 leading-tight" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+									<span class="text-sm">{{ __('Remember Me') }}</span>
+							</label>
+					</div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+					<div class="flex items-center justify-between mt-4">
+						<button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+								{{ __('Login') }}
+						</button>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+						@if (Route::has('password.request'))
+							<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
+									{{ __('Forgot Your Password?') }}
+							</a>
+						@endif
+					</div>
+			</form>
+	</div>
 </div>
 @endsection
--- resources/views/equipment/create.blade.php
+++ resources/views/equipment/create.blade.php
@@ -2,57 +2,59 @@
 
 @section('content')
 <section>
-    <div class="container">
-				@if ($errors->any())
-					<div class="bg-red-200 border border-red-300 font-medium mb-6 px-4 py-3 rounded text-red-700 text-sm">
-						<ul>
-							@foreach ($errors->all() as $error)
-								<li>{{ $error }}</li>
-							@endforeach
-						</ul>
-					</div>
-				@endif
-				
-				<h3 class="font-medium text-2xl text-gray-800">Add Equipment</h3>
+    <div class="container flex justify-center">
+				<div class="w-2/3">
+					@if ($errors->any())
+						<div class="bg-red-200 border border-red-300 font-medium mb-6 px-4 py-3 rounded text-red-700 text-sm">
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+					
+					<h3 class="font-medium text-2xl text-gray-800">Add Equipment</h3>
 
-				<div class="p-4 bg-white shadow rounded mt-6">
-					<form method="POST" action="{{ route('admin.equipments.store') }}">
-						@csrf
-						<div class="flex flex-wrap -mx-4 -mt-4">
-							<div class="w-1/2 px-4 mt-4">
-								<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">Name</label>
-								<input type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" name="name">
-								@error('name')
-									<span class="text-xs text-red-500">{{ $message }}</span>
-								@enderror
-							</div>
-							<div class="w-1/2 px-4 mt-4">
-								<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="type">Type</label>
-								<div class="relative">
-									<select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="type_id" id="type">
-										@foreach ($types as $option)
-										<option value="{{ $option->id }}">{{ $option->name }}</option>
-										@endforeach
-									</select>
-									<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-											<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg>
+					<div class="bg-white mt-6 px-8 py-8 rounded shadow">
+						<form method="POST" action="{{ route('admin.equipments.store') }}">
+							@csrf
+							<div class="flex flex-wrap -mx-4 -mt-4">
+								<div class="w-1/2 px-4 mt-4">
+									<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">Name</label>
+									<input type="text" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" name="name">
+									@error('name')
+										<span class="text-xs text-red-500">{{ $message }}</span>
+									@enderror
+								</div>
+								<div class="w-1/2 px-4 mt-4">
+									<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="type">Type</label>
+									<div class="relative">
+										<select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="type_id" id="type">
+											@foreach ($types as $option)
+											<option value="{{ $option->id }}">{{ $option->name }}</option>
+											@endforeach
+										</select>
+										<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+												<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg>
+										</div>
 									</div>
 								</div>
+								<div class="w-1/2 px-4 mt-4">
+									<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="description">Description</label>
+									<textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="description" name="description" rows="4"></textarea>
+								</div>
 							</div>
-							<div class="w-1/2 px-4 mt-4">
-								<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="description">Description</label>
-								<textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="description" name="description" rows="4"></textarea>
+							<div class="mt-4 flex items-center">
+								<button class="bg-blue-200 font-medium leading-snug px-4 py-2 rounded text-blue-700 text-sm" type="submit">
+									Submit
+								</button>
+								<a href="{{ route('admin.equipments.index') }}" class="ml-4 bg-gray-300 text-gray-700 font-medium py-2 px-4 rounded text-sm leading-snug inline-block">
+									Cancel
+								</a>
 							</div>
-						</div>
-						<div class="mt-4 flex items-center">
-							<button class="bg-blue-200 font-medium leading-snug px-4 py-2 rounded text-blue-700 text-sm" type="submit">
-								Submit
-							</button>
-							<a href="{{ route('admin.equipments.index') }}" class="ml-4 bg-gray-300 text-gray-700 font-medium py-2 px-4 rounded text-sm leading-snug inline-block">
-								Cancel
-							</a>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
     </div>
 </section>
